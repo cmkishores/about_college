4fb3f890a3c6a01fe9831609e50530ea1fe518c6
cmkishores <cmkishores@gmail.com>
2019-09-09 23:14:11 +0530

    Added different pages for the website and made their url maps.

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,11 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
 	template_name = 'index.html'
+class ContactPageView(TemplateView):
+	template_name = 'contact.html'
+class DeptPageView(TemplateView):
+	template_name = 'dept.html'
+class AboutPageView(TemplateView):
+	template_name = 'about.html'
 
 # Create your views here.
